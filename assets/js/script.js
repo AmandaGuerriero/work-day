ed7d5f25d35c4b976c4d2522bb48ce38bb759686
Amanda Guerriero <ag.codingclass@gmail.com>
2020-09-03 22:36:45 -0700

    Saved Tasks to Local Storage, but need to retrieve and print them

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,21 @@
 var day = document.getElementById("currentDay");
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-// Style time block based on time of day
+// Save Tasks to Local Storage
+$(".saveBtn").on("click", function () {
+    var editText = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, editText);
+    console.log(time, editText);
+})
+
+// Retrieve Task from Local Storage
+$("#hour9am" .description).val(localStorage.getItem("hour9am"));
+
+
+// // Style Hours based on past, present, future
+// function hourCheck()
+
+// if (hourRow < currentHour) {
+//     $(this).addClass("past");
+// }
