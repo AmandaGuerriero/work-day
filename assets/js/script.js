6078c861b59f21b1ed96bcca8350762a01e230b0
Amanda Guerriero <ag.codingclass@gmail.com>
2020-09-04 17:32:30 -0700

    Cleaned up code with my tutor to get retrieval from local storage to work and conditional statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,54 @@
-// Print current day
-var day = document.getElementById("currentDay");
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
+$(document).ready(function (){
+    // Print current day
+    var day = document.getElementById("currentDay");
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-// Save Tasks to Local Storage
-$(".saveBtn").on("click", function () {
-    var editText = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, editText);
-    console.log(time, editText);
-})
-
-// Retrieve Task from Local Storage
-$("#hour9am" .description).val(localStorage.getItem("hour9am"));
-
-
-// Style Hours based on past, present, future
-var hourCheck = function() {
-    var currentTime = moment().format("hA");
-    console.log(currentTime);
-    
-    $(".time-block").each(function () {
-        var hourRow = ("#hour9am")
-        console.log(hourRow, currentTime);
-        if (hourRow < currentTime) {
-            $(this).addClass("past");
-        }
-        else if (hourRow === currentTime) {
-            $(this).addClass("present");
-        }
-        else {
-            $(this).addClass("future");
-        }
+    // Save Tasks to Local Storage
+    $(".saveBtn").on("click", function () {
+        var editText = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, editText);
+        console.log(time, editText);
     })
-};
 
-hourCheck ();
+    // Retrieve Task from Local Storage
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+    // Style Hours based on past, present, future
+    var hourCheck = function() {
+        var currentTime = moment().hours();
+        console.log(currentTime);
+        
+        $(".time-block").each(function () {
+            var hourRow = parseInt($(this).attr("id").split("-")[1]);
+            console.log(hourRow, currentTime);
+            if (hourRow < currentTime) {
+                $(this).addClass("past");
+            }
+            else if (hourRow === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    };
+
+    hourCheck();
+
+    // Set Interval
+
+    var interval = setInterval(hourCheck, 15000);
+
+});
